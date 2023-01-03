af402466b5c46f75afcd250eb9dddc988402bcf5
pathakr007 <pathak007.react@gmail.com>
2023-01-03 23:36:57 +0530

    taking user's input

---
+++ app.js
@@ -0,0 +1,7 @@
+var readlineSync = require('readline-sync');
+
+console.log("Are you a true fan of Amitabh Bachchan?");
+
+var userName=readlineSync.question("\nPlease enter your name:\n")
+
+console.log("\n\nHi,"+userName+"!"+" Welcome \n");
